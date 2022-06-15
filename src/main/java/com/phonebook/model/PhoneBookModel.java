fe878493bb67e0e2ece86ec5b03c075355263f41
kavitakathe <kavitavidhate16@gmail.com>
2022-06-16 01:15:41 +0530

    phn1

---
+++ src/main/java/com/phonebook/model/PhoneBookModel.java
@@ -0,0 +1,5 @@
+package com.phonebook.model;
+
+public class PhoneBookModel {
+
+}
---
+++ src/main/java/com/phonebook/repository/PhoneBookRepository.java
@@ -0,0 +1,7 @@
+package com.phonebook.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhoneBookRepository  extends JpaRepository{
+
+}
